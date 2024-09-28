0d1d6968948159e870c8993fcd041699a0e331f6
Elvis <steevenbailon23@gmail.com>
2024-09-27 21:00:41 -0500

    inicio del diseño

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./Pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./Pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'friends',
+    loadChildren: () => import('./Pages/friends/friends.module').then( m => m.FriendsPageModule)
+  },
+  {
+    path: 'people',
+    loadChildren: () => import('./Pages/people/people.module').then( m => m.PeoplePageModule)
+  },
+  {
+    path: 'estados',
+    loadChildren: () => import('./Pages/estados/estados.module').then( m => m.EstadosPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
