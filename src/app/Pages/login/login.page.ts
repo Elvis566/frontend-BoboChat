95c5535d488f28db87deefc951eb702ffa182500
Elvis <steevenbailon23@gmail.com>
2024-09-30 21:10:52 -0500

    incorporacion del metodo login

--- src/app/Pages/login/login.page.ts
+++ src/app/Pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/Servicios/api.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private apiS: ApiService, private router:Router) { }
 
   ngOnInit() {
+  }
+
+  login(password: any, email:any){
+    this.apiS.login(password.value, email.value).subscribe({
+      next:(data:any)=>{
+        localStorage.setItem('userId', data.user.id);
+        localStorage.setItem('apodoUser', data.user.apodo);
+        localStorage.setItem('descripcionUser', data.user.descripcion);
+        localStorage.setItem('avatarIdUser', data.user.avatar_id);
+
+        this.router.navigate(['/home']);
+        
+      },error:(e:any)=>{
+        console.log(e);
+      }
+    })
   }
 
 }
